c10421821039091c681192195225c92a8a4f6bd1
Besufekad-HAZ <besurye@gmail.com>
2025-04-11 11:54:57 +0300

    Setup the Project with the latest changes

---
+++ app/about/page.js
@@ -0,0 +1,15 @@
+export default function AboutPage() {
+  return (
+    <main style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+      <h1>About Us</h1>
+      <p>
+        Welcome to our food website! We are passionate about sharing delicious
+        recipes and food inspiration.
+      </p>
+      <p>
+        Our mission is to help you discover new tastes and culinary experiences
+        from around the world.
+      </p>
+    </main>
+  );
+}
