ea493b4a3ed2e4d069ae024ada20d8e2ad2f8243
Besufekad-HAZ <besurye@gmail.com>
2025-04-11 12:54:05 +0300

    Setup the Project and start working on the food app fully

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
         Time to get started!
       </h1>
     </main>
   );
 }
-// This is a server component
-// Server components are rendered on the server by default
